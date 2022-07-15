2dfafbba0017cced6a7d034044b57bd2221c4c71
ptl203 <paullahiff@gmail.com>
2022-07-14 17:58:09 -0700

    Working Minimal System

--- src/alpha/src/alpha.cpp
+++ src/alpha/src/alpha.cpp
@@ -14,6 +14,7 @@ class Alpha : public rclcpp::Node
             service_ = this->create_service<ball_interfaces::srv::StartSequence>("start_trigger", std::bind(&Alpha::start, this, _1, _2));
             RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Alpha Node Started");
             publisher_ = this->create_publisher<ball_interfaces::msg::PubSub>("ab_connect", 10);
+            subscriber_ = this->create_subscription<ball_interfaces::msg::PubSub>("ca_connect", 10, std::bind(&Alpha::ca_callback, this, _1));
         }
 
     private:
@@ -27,7 +28,18 @@ class Alpha : public rclcpp::Node
             message.message = "Throwing to Bravo";
             RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.message.c_str());
             publisher_->publish(message);
-        }   
+        }  
+
+        void ca_callback(const ball_interfaces::msg::PubSub::SharedPtr msg) const
+        {
+            RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->message.c_str());
+            auto message = ball_interfaces::msg::PubSub();
+            message.message = "Throwing to Bravo";
+            RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.message.c_str());
+            publisher_->publish(message);
+        }
+
+        rclcpp::Subscription<ball_interfaces::msg::PubSub>::SharedPtr subscriber_; 
         rclcpp::Service<ball_interfaces::srv::StartSequence>::SharedPtr service_;
         rclcpp::Publisher<ball_interfaces::msg::PubSub>::SharedPtr publisher_;
 };
